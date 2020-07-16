ed1b29330ea1071da3f34e9f0b3fdf7c5bd9b30a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-16 20:53:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,5 +22,8 @@ def printer(attendees)
     batch_badge_creator(attendees).each do |badge|
       puts badge
     end
+    assign_rooms(attendees).each do |assignment|
+      puts assignment
+    end
   #end
 end
