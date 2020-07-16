5c6aeb98799841cf0fa321f7f7dc5394de841270
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-16 19:41:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_maker(array)
+def batch_badge_maker(attendees)
   new_array = []
   array.each do |name|
     badge = "Hello, my name is #{name}."
